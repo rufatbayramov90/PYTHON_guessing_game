5501d72e303b14a3b8b8eb817d98e146dd1d3f6f
rufatbayramov90 <rufatbyrmv@gmail.com>
2023-10-16 15:46:32 +0400

    difficulty level

--- main.py
+++ main.py
@@ -17,15 +17,19 @@ def check_answer(guess, answer):
 def set_difficulty():
     level = input("Choose a difficulty.Type 'easy' or 'hard': ") 
     if level == "easy":
-        turns = easy_level_turns
+        return easy_level_turns
     else:
-        turns = hard_level_turns
+        return  hard_level_turns
 
 
 print("Welcome to the Number Guessing Game")
 print("I am thinking of a number between 1 and 100")
-
 answer = randint(1,100)
-
+print(f"The correct answer is {answer}")
+turns = set_difficulty()
+print(f"You have {turns} attempts to guess the number")
 guess = int(input("Make a guess: "))
+
+check_answer(guess)
+
  
