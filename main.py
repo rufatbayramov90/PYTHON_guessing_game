b2da6db5a23cb928dc5251bb9eb0f14547df809f
rufatbayramov90 <rufatbyrmv@gmail.com>
2023-10-16 16:11:58 +0400

    number guessing game

--- main.py
+++ main.py
@@ -3,12 +3,15 @@ from random import randint
 easy_level_turns = 10
 hard_level_turns = 5
 
+turns = 0
 #Function to check user's guess against actual answer
-def check_answer(guess, answer):
+def check_answer(guess, answer,turns):
     if guess > answer:
         print("Too high")
+        return turns - 1
     elif guess < answer:
         print("Too low")
+        return turns - 1 
     else:
         print(f"You got it! The answer was {answer}")
 
@@ -21,15 +24,28 @@ def set_difficulty():
     else:
         return  hard_level_turns
 
+def game():
+    print("Welcome to the Number Guessing Game")
+    print("I am thinking of a number between 1 and 100")
+    answer = randint(1,100)
+    print(f"The correct answer is {answer}")
 
-print("Welcome to the Number Guessing Game")
-print("I am thinking of a number between 1 and 100")
-answer = randint(1,100)
-print(f"The correct answer is {answer}")
-turns = set_difficulty()
-print(f"You have {turns} attempts to guess the number")
-guess = int(input("Make a guess: "))
+    turns = set_difficulty()
+    while guess != answer:
+        print(f"You have {turns} attempts to guess the number")
 
-check_answer(guess)
+        guess = 0
+        while guess != answer:
+            guess = int(input("Make a guess: "))
+            turns = check_answer(guess,answer,turns)
+            if turns == 0:
+                print("You have run out of guesses, you lose")
+                return
+            elif guess != answer:
+                print("Guess again")
+
+
+
+game()
 
  
