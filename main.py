3ce64f5de189f5944d758c78d7ae6ed898f0237f
rufatbayramov90 <rufatbyrmv@gmail.com>
2023-10-16 15:39:04 +0400

    Function to adjust difficulty

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from random import randint
 
+easy_level_turns = 10
+hard_level_turns = 5
+
 #Function to check user's guess against actual answer
 def check_answer(guess, answer):
     if guess > answer:
@@ -9,7 +12,15 @@ def check_answer(guess, answer):
     else:
         print(f"You got it! The answer was {answer}")
 
-        
+
+#Make function to set difficulty
+def set_difficulty():
+    level = input("Choose a difficulty.Type 'easy' or 'hard': ") 
+    if level == "easy":
+        turns = easy_level_turns
+    else:
+        turns = hard_level_turns
+
 
 print("Welcome to the Number Guessing Game")
 print("I am thinking of a number between 1 and 100")
